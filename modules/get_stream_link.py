265693578f04ef014ef4461db9c1b3b687d0db17
Ollie <141184120+O11iee@users.noreply.github.com>
2024-06-24 13:36:31 +0100

    error handling and reduced webdriver timeout

--- modules/get_stream_link.py
+++ modules/get_stream_link.py
@@ -7,8 +7,8 @@ import logging
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
+from selenium.common.exceptions import TimeoutException
 from pathlib import Path
-
 
 # Setup basic logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -25,30 +25,29 @@ lock_files_dir = script_dir / 'lock_files'
 binaries_dir = script_dir / 'binaries'
 
 def load_cookies():
-    with open(json_dir / "cookies.json", "r") as file:
-        cookies = json.load(file)
-    return cookies
+    try:
+        with open(json_dir / "cookies.json", "r") as file:
+            cookies = json.load(file)
+        return cookies
+    except Exception as e:
+        logging.error(f"Error loading cookies: {e}")
+        return []
 
 def write_stream_links_to_file(username, stream_link):
     stream_links_path = json_dir / "stream_links.json"
     try:
-        data = {}  # Initialize an empty dictionary to hold stream links
-        # Check if the file exists and is not empty before attempting to read
+        data = {}
         if stream_links_path.exists() and os.path.getsize(stream_links_path) > 0:
             with open(stream_links_path, "r") as file:
                 try:
                     data = json.load(file)
                 except json.JSONDecodeError:
                     logging.error("Invalid JSON content detected. Creating a new stream links file.")
-                    # If the file contains invalid JSON, proceed with an empty dictionary
-        # Update or add the stream link
         data[username] = stream_link
-        # Always write back to the file, creating it if necessary
         with open(stream_links_path, "w") as file:
             json.dump(data, file, indent=4)
-    except Exception as e:  # Catching a more general exception might be more appropriate here
+    except Exception as e:
         logging.error(f"Error updating stream links JSON: {e}")
-
 
 def correct_url_format(url):
     return url.replace("\\u002F", "/").replace("\\u0026", "&")
@@ -83,9 +82,13 @@ def find_stream_link(page_source, username, force_flv_users):
     return None
 
 def load_force_flv_users():
-    with open("./json/force_flv.json", "r") as file:
-        data = json.load(file)
-    return data.get("force_flv_users", [])
+    try:
+        with open(json_dir / "force_flv.json", "r") as file:
+            data = json.load(file)
+        return data.get("force_flv_users", [])
+    except Exception as e:
+        logging.error(f"Error loading force FLV users: {e}")
+        return []
 
 def start_browser():
     geckodriver_path = binaries_dir / "geckodriver.exe"    
@@ -99,6 +102,7 @@ def start_browser():
     options.set_preference("network.http.use-cache", False)
     service = Service(executable_path=geckodriver_path)
     driver = webdriver.Firefox(service=service, options=options)
+    driver.set_page_load_timeout(30)
     return driver
 
 def auth(driver):
@@ -124,31 +128,22 @@ def read_stream_links(path=json_dir / "live_users.json"):
         return [StreamLink(**item) for item in data]
     except json.JSONDecodeError:
         logging.error(f"Empty or invalid JSON in {path}.")
-        return []  # Return an empty list or some default value
-
+        return []
 
 def clear_old_stream_links(active_usernames):
     stream_links_path = json_dir / "stream_links.json"
     try:
-        # Check if the stream links file exists and read it
         if stream_links_path.exists():
             with open(stream_links_path, "r") as file:
                 data = json.load(file)
-            
-            # Filter out inactive stream links
             filtered_data = {username: link for username, link in data.items() if username in active_usernames}
-            
-            # Write the filtered links back to the file
             with open(stream_links_path, "w") as file:
                 json.dump(filtered_data, file, indent=4)
-                
             logging.info("Cleared old stream links successfully.")
         else:
             logging.info("Stream links file does not exist. No need to clear old links.")
     except json.JSONDecodeError as e:
         logging.error(f"Error reading or updating stream links JSON: {e}")
-
-
 
 def process_user(driver, user, force_flv_users):
     lock_file_path = lock_files_dir / f"{user.username}.lock"
@@ -159,7 +154,7 @@ def process_user(driver, user, force_flv_users):
         driver.get(f"view-source:{user.stream_link}")
         page_source = driver.page_source
         room_id = find_room_id(page_source)
-                
+
         if room_id:
             webcast_url = f"https://webcast.tiktok.com/webcast/room/info/?aid=1988&room_id={room_id}"
             driver.get(f"view-source:{webcast_url}")
@@ -171,8 +166,8 @@ def process_user(driver, user, force_flv_users):
                 logging.info(f"No stream link found for {user.username}")
         else:
             logging.info(f"No room ID found for {user.username}. Passing firewall challenge...")
-            driver.get(user.stream_link)  # Retrying without 'view-source:' prefix
-            time.sleep(3)  # Wait for JavaScript execution
+            driver.get(user.stream_link)
+            time.sleep(3)
             page_source = driver.page_source
             room_id = find_room_id(page_source)
             if room_id:
@@ -186,10 +181,10 @@ def process_user(driver, user, force_flv_users):
                     logging.info(f"No stream link found for {user.username} after retry.")
             else:
                 logging.info(f"No room ID found for {user.username} after retry.")
+    except TimeoutException as e:
+        logging.error(f"Timeout error processing {user.username}: {e}")
     except Exception as e:
         logging.error(f"Error processing {user.username}: {e}")
-
-
 
 def main():
     logging.info("Starting the program...")
@@ -197,19 +192,27 @@ def main():
     auth(driver)
 
     while True:
-        logging.info("Checking for updates.")
-        force_flv_users = load_force_flv_users()
-        active_stream_links = read_stream_links()
-        active_usernames = [user.username for user in active_stream_links]
-        if active_stream_links:
-            for user in active_stream_links:
-                process_user(driver, user, force_flv_users)
-        else:
-            logging.info("No active stream links found.")
-        
-        clear_old_stream_links(set(active_usernames))
-        time.sleep(5)
-
+        try:
+            logging.info("Checking for updates.")
+            force_flv_users = load_force_flv_users()
+            active_stream_links = read_stream_links()
+            active_usernames = [user.username for user in active_stream_links]
+            if active_stream_links:
+                for user in active_stream_links:
+                    process_user(driver, user, force_flv_users)
+            else:
+                logging.info("No active stream links found.")
+            
+            clear_old_stream_links(set(active_usernames))
+            time.sleep(5)
+        except TimeoutException as e:
+            logging.error(f"Timeout error in main loop: {e}")
+            driver.quit()
+            driver = start_browser()
+            auth(driver)
+        except Exception as e:
+            logging.error(f"Error in main loop: {e}")
+        time.sleep(1)
 
 if __name__ == "__main__":
     main()
